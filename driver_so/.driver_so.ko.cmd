923a7ffdadd49de4b9d8a3ef2dc9117f985322d5
Henrique <henscalon@hotmail.com>
2016-06-18 21:32:55 -0300

    commit inicial

--- driver_so/.driver_so.ko.cmd
+++
@@ -1,1 +0,0 @@
-cmd_/opt/trabalho_so/driver_so/driver_so.ko := ld -r -m elf_x86_64 -T ./scripts/module-common.lds --build-id  -o /opt/trabalho_so/driver_so/driver_so.ko /opt/trabalho_so/driver_so/driver_so.o /opt/trabalho_so/driver_so/driver_so.mod.o
--- driver_so/.tmp_versions/driver_so.mod
+++
@@ -1,2 +0,0 @@
-/opt/trabalho_so/driver_so/driver_so.ko
-/opt/trabalho_so/driver_so/driver_so.o
